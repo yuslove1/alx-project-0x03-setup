da331fbcb0d97f903a0be2d041c523e2c5ab9d65
yuslove1 <adexyuslove@gmail.com>
2025-01-14 07:06:25 -0800

    fix: minor bug fixed

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,11 +1,7 @@
-import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { LayoutProps } from "@/interface";
 
-// Defines the properties for the Layout component.
-interface LayoutProps {
-  children: ReactNode; // Children to be rendered within the layout (the main content of the page).
-}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
